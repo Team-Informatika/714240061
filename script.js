0c46aab1726643b559aeebe479fc866d6e3b9647
kaizhr <zidanramadhan950@gmail.com>
2025-01-02 11:37:40 +0700

    v

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 import {renderHTML} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js"
 
 renderHTML('content','home.html');
+onClick("github",myGithub);
+function myGithub() {
+    window.open('https://github.com/kaizhr', 'blank');
+}
+onClick("whatsapp",mywhatsapp);
+function mywhatsapp() {
+    window.open('https://whatsapp.com/6283114925705', 'blank');
+}
+onClick("instagram",myinstagram);
+function myinstagram() {
+    window.open('https://instagram.com/kaizhr.45', 'blank');
+}

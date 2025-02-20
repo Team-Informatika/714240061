f39bb8840749c47aa3e0a12fe34a61a2845ca506
zidan hairra ramadan <zidanramadhan950@gmail.com>
2025-02-20 21:06:55 +0700

    coba

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ onHashChange(ada);
 function ada() {
     console.log(getHash());
     const hashpath = getHash();
-    if (hashpath === "profile") {
+    if (hashpath === "") {
         console.log("ini gan, sudah muncul datanya😊");
         renderHTML("container", "profile.html", renderDataDariJson);
     }
